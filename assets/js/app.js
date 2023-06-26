cf027d5b789cd590443f5507262e5d657139ac64
Helen Chong <119173961+helenclx@users.noreply.github.com>
2023-06-26 11:30:50 +0800

    Mobile-first workflow site layout

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,7 +16,7 @@ const ul = document.querySelector("nav ul");
 const nav = document.querySelector("nav");
 
 burger.addEventListener("click", () => {
-    ul.classList.toggle("show");
+    ul.classList.toggle("nav-show");
 });
 
 // Close hamburger menu when a link is clicked
@@ -26,6 +26,6 @@ const navLink = document.querySelectorAll(".nav-link");
 
 navLink.forEach((link) =>
     link.addEventListener("click", () => {
-        ul.classList.remove("show");
+        ul.classList.remove("nav-show");
     })
 );
