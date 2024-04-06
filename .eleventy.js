64b6f58ca8193522afdc9492c48ce46f9fb566fa
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-04-06 22:18:10 +0800

    Configurate my Eleventy portfolio site

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("./src/assets/");
+    eleventyConfig.addWatchTarget("./src/assets/");
+
+    return {
+        dir: {
+            input: "src"
+        }
+    };
+};
