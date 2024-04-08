82bf5827e0669f2b59be62596805a664a080d1e7
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-04-08 18:07:30 +0800

    Install Eleventy Image plug-in

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,30 @@
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, sizes) {
+    let metadata = await Image(`./src${src}`, {
+        widths: [300, 800, null],
+        formats: ["avif", "jpeg"],
+        urlPath: "/images/",
+        outputDir: "./_site/images/"
+    });
+
+    let imageAttributes = {
+        alt,
+        sizes,
+        loading: "lazy",
+        decoding: "async"
+    };
+
+    return Image.generateHTML(metadata, imageAttributes);
+}
+
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/assets/");
     eleventyConfig.addWatchTarget("./src/assets/");
+
+    eleventyConfig.addNunjucksAsyncShortcode("EleventyImage", imageShortcode);
+
+    eleventyConfig.addShortcode("current_year", () => `${new Date().getFullYear()}`);
 
     return {
         dir: {
