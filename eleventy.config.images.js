4fe3410be1d664fd7bb983652e5030af59f2a7aa
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-04-09 15:21:07 +0800

    Rename variable for the eleventy image plug-in

--- eleventy.config.images.js
+++ eleventy.config.images.js
@@ -1,7 +1,7 @@
-const Image = require("@11ty/eleventy-img");
+const eleventyImage = require("@11ty/eleventy-img");
 
 async function imageShortcode(src, alt, sizes) {
-    let metadata = await Image(`./src${src}`, {
+    let metadata = await eleventyImage(`./src${src}`, {
         widths: [300, 800, null],
         formats: ["avif", "jpeg"],
         urlPath: "/images/",
@@ -15,7 +15,7 @@ async function imageShortcode(src, alt, sizes) {
         decoding: "async"
     };
 
-    return Image.generateHTML(metadata, imageAttributes);
+    return eleventyImage.generateHTML(metadata, imageAttributes);
 }
 
 module.exports = function(eleventyConfig) {
