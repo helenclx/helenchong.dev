8dd524255e961a6a25e326725e7f3edfc8b376bc
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-04-13 15:26:39 +0800

    Add site metadata file

---
+++ src/_data/sitemeta.js
@@ -0,0 +1,11 @@
+module.exports = {
+	siteName: "Helen Chong | Front-End Web Developer",
+	siteUrl: "https://helenclx.github.io/" || "http://localhost:8080",
+	siteLocale: "en_MY",
+	siteDescription: "Helen Chong's developer portfolio and blog website.",
+	siteAuthor: {
+		name: "Helen Chong",
+		email: "helenclx@outlook.com",
+		url: "https://helenclx.github.io/about/"
+	}
+}
