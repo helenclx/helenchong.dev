f84477c69701cb0fdccd0f8d9792ac6c090a6d56
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-04-15 17:13:25 +0800

    Install eleventy-plugin-embed-everything

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,6 +3,7 @@ const { EleventyRenderPlugin } = require("@11ty/eleventy");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const metagen = require('eleventy-plugin-metagen');
 const emojiReadTime = require("@11tyrocks/eleventy-plugin-emoji-readtime");
+const embedEverything = require("eleventy-plugin-embed-everything");
 
 // App Plug-Ins
 const pluginImages = require("./eleventy.config.images.js");
@@ -16,6 +17,9 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(pluginRss);
     eleventyConfig.addPlugin(metagen);
     eleventyConfig.addPlugin(emojiReadTime);
+    eleventyConfig.addPlugin(embedEverything, {
+        add: ['soundcloud']
+    });
 
     // App plug-ins
     eleventyConfig.addPlugin(pluginImages);
