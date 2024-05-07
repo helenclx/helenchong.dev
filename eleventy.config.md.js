2f2311422f87f74e930f0792416d24eacec3d0ed
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-05-07 13:45:47 +0800

    Install markdown-it-attrs and markdown-it-bracketed-spans plugins

--- eleventy.config.md.js
+++ eleventy.config.md.js
@@ -9,4 +9,22 @@ const markdownIt = require("markdown-it");
 module.exports = function (eleventyConfig) {
     // Installed plugins
     eleventyConfig.addPlugin(embedEverything, { add: ['soundcloud'] });
+
+    /* Markdown Overrides */
+    let markdownLibrary = markdownIt({
+        html: true,
+    })
+        .use(require("markdown-it-attrs"))
+        .use(require("markdown-it-bracketed-spans"))
+
+    // Paired shortcode: custom container
+    eleventyConfig.addPairedShortcode('container', (children, el, className) => {
+        const classMarkup = className ? ` class="${className}"` : "";
+        const content = markdownLibrary.render(children);
+        return `<${el}${classMarkup}>${content}</${el}>`;
+    });
+
+    /* This is the part that tells 11ty to swap to our custom config */
+    eleventyConfig.setLibrary("md", markdownLibrary);
+
 }
