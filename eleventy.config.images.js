6c7e91699d35f895c78e91d2eb5467da8bc8f7c4
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-07-19 14:04:54 +0800

    Use Eleventy Image Transform plugin for images

--- eleventy.config.images.js
+++ eleventy.config.images.js
@@ -1,23 +1,12 @@
-import eleventyImage, { generateHTML } from "@11ty/eleventy-img";
-
-async function imageShortcode(src, alt, sizes) {
-    let metadata = await eleventyImage(`./src${src}`, {
-        widths: [300, 800, null],
-        formats: ["avif", "jpeg"],
-        urlPath: "/assets/images/",
-        outputDir: "./_site/assets/images/"
-    });
-
-    let imageAttributes = {
-        alt,
-        sizes,
-        loading: "lazy",
-        decoding: "async"
-    };
-
-    return generateHTML(metadata, imageAttributes);
-}
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 
 export default function(eleventyConfig) {
-    eleventyConfig.addNunjucksAsyncShortcode("EleventyImage", imageShortcode);
+    eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+		extensions: "html",
+		formats: ["avif", "jpeg"],
+		defaultAttributes: {
+			loading: "lazy",
+			decoding: "async",
+		},
+	});
 }
