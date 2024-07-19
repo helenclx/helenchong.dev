1b0d0a14383715792cc88a3cd444ae3703d2793f
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-07-19 09:07:00 +0800

    Use export variables for sitemeta data

--- src/_data/sitemeta.js
+++ src/_data/sitemeta.js
@@ -1,16 +1,14 @@
-export default {
-	siteName: "Helen Chong | Front-End Web Developer",
-	siteUrl: "https://helenchong.dev" || "http://localhost:8080",
-	siteDomain: "helenchong.dev",
-	siteLocale: "en_MY",
-	siteDescription: "Helen Chong's developer portfolio and blog website.",
-	siteAuthor: {
-		name: "Helen Chong",
-		email: "contact@helenchong.dev",
-		url: "https://helenchong.dev/about"
-	},
-	siteBlog: {
-		title: "Helen Chong's Developer Blog",
-		desc: "Helen Chong talks about tech, coding and development",
-	}
-}
+export const siteName = "Helen Chong | Front-End Web Developer";
+export const siteDomain = "helenchong.dev";
+export const siteUrl = "https://" + siteDomain || "http://localhost:8080";
+export const siteAuthor = {
+	name: "Helen Chong",
+	email: "contact@helenchong.dev",
+	url: siteUrl + "/about"
+};
+export const siteDescription = siteAuthor.name + "'s developer portfolio and blog website.";
+export const siteLocale = "en_MY";
+export const siteBlog = {
+	title: siteAuthor.name + "'s Developer Blog",
+	desc: siteAuthor.name + " talks about tech, coding and development",
+};
