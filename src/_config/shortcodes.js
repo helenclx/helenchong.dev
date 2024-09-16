bc2ca24dd20622f0980403c06e07f57ac63c6b7f
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-09-16 08:58:33 +0800

    Add manual heading anchor paired shorcode

--- src/_config/shortcodes.js
+++ src/_config/shortcodes.js
@@ -1,4 +1,12 @@
 export default function(eleventyConfig) {
     // Current year
     eleventyConfig.addShortcode("currentYear", () => `${new Date().getFullYear()}`);
+
+    // Manual heading anchor
+    eleventyConfig.addPairedShortcode('headingAnchor', (title, hLevel, id=slugify(title)) => {
+        return `<div class="heading-wrapper h${hLevel}">
+            <h${hLevel} id="${id}">${title}</h${hLevel}>
+            <a class="heading-anchor" href="#${id}" aria-labelledby="${id}"><span hidden="">#</span></a>
+        </div>`;
+    });
 }
