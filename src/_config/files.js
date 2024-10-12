3c4b513c98419747c59fb1c944e4dc155a615389
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-10-12 16:31:50 +0800

    Improve setup of favicon

--- src/_config/files.js
+++ src/_config/files.js
@@ -3,6 +3,7 @@ export default function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/assets/");
     eleventyConfig.addWatchTarget("./src/assets/");
     eleventyConfig.addPassthroughCopy({
+        "./src/assets/icons/favicon/favicon.ico": "/",
         "node_modules/@zachleat/details-utils/details-utils.js": "assets/js/details-utils.js",
     });
 }
