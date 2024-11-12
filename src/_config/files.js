4cc01ef77d2737c3877ff5ed0058afab96e4b1d7
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-11-12 11:58:16 +0800

    Move favicon folder to main assets folder

--- src/_config/files.js
+++ src/_config/files.js
@@ -3,7 +3,7 @@ export default function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/assets/");
     eleventyConfig.addWatchTarget("./src/assets/");
     eleventyConfig.addPassthroughCopy({
-        "./src/assets/graphics/favicon/favicon.ico": "/favicon.ico",
+        "./src/assets/favicon/favicon.ico": "/favicon.ico",
         "node_modules/@zachleat/details-utils/details-utils.js": "assets/js/details-utils.js",
     });
 }
