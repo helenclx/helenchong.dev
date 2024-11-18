8eecbc85c262d0ef1b220d2de7a6b2343102543a
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-11-18 21:06:10 +0800

    Add chat methods

--- src/_data/sitemeta.js
+++ src/_data/sitemeta.js
@@ -4,7 +4,9 @@ export const siteUrl = "https://" + siteDomain || "http://localhost:8080";
 export const siteAuthor = {
 	name: "Helen Chong",
 	email: "contact@helenchong.dev",
-	url: siteUrl + "/about"
+	url: siteUrl + "/about",
+	matrix: "@helenchong:omg.lol",
+	signal: "helenchong.08"
 };
 export const siteDescription = siteAuthor.name + "'s developer portfolio and blog website.";
 export const siteLang = "en";
