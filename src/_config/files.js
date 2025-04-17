cd9b21494490a80b67908dda8f7ca300d944f4d8
Helen Chong <119173961+helenclx@users.noreply.github.com>
2025-04-17 20:20:34 +0800

    Remove favicon configurations

--- src/_config/files.js
+++ src/_config/files.js
@@ -4,6 +4,7 @@ export default function(eleventyConfig) {
     eleventyConfig.addWatchTarget("./src/assets/");
     eleventyConfig.addPassthroughCopy({
         "./src/assets/favicon/favicon.ico": "/favicon.ico",
+        "./src/assets/favicon/apple-touch-icon.png": "/apple-touch-icon.png",
         "node_modules/@zachleat/details-utils/details-utils.js": "assets/js/details-utils.js",
     });
 }
