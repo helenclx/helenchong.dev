32522e84a848882b81dd2eeeeb16b842d8cb1329
Helen Chong <contact@helenchong.dev>
2026-07-18 20:25:25 +0800

    data/sitemeta.js: use template literals for values

--- src/_data/sitemeta.js
+++ src/_data/sitemeta.js
@@ -1,25 +1,24 @@
 export const siteName = "Helen Chong, Web Developer";
 export const siteDomain = "helenchong.dev";
-export const siteUrl = "https://" + siteDomain || "http://localhost:8080";
+export const siteUrl = `https://${siteDomain}` || "http://localhost:8080";
 export const siteAuthor = {
 	name: "Helen Chong",
 	email: "contact@helenchong.dev",
 	emailEncoded:
 		'<span class="email-encoded">contact@helenchong<b>.mail</b>.dev</span>',
 	emailDecoyUrl: "emailme/",
-	url: siteUrl + "/about",
+	url: `${siteUrl}/about`,
 	fediverse: {
 		handle: "@helenchong@social.lol",
 		url: "https://social.lol/@helenchong",
 	},
 	signal: "helenchong.08",
 };
-export const siteDescription =
-	siteAuthor.name + "'s developer portfolio and blog website.";
+export const siteDescription = `${siteAuthor.name}'s developer portfolio and blog website.`;
 export const siteLang = "en";
 export const siteLocale = "en_MY";
 export const siteBlog = {
-	title: siteAuthor.name + "'s Developer Blog",
-	desc: siteAuthor.name + " talks about tech, coding and development.",
+	title: `${siteAuthor.name}'s Developer Blog`,
+	desc: `#{siteAuthor.name} talks about tech, coding and development.`,
 };
 export const feedPath = "/blog/feed.xml";
