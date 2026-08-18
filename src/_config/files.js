ebfc76deb2f362d71d2c92a3940afe0011e0c5df
Helen Chong <contact@helenchong.dev>
2026-08-18 12:40:43 +0800

    config/files.js: streamline file passthrough of favicons

--- src/_config/files.js
+++ src/_config/files.js
@@ -3,8 +3,7 @@ export default function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("./src/assets/");
 	eleventyConfig.addWatchTarget("./src/assets/");
 	eleventyConfig.addPassthroughCopy({
-		"./src/assets/favicon/favicon.ico": "/favicon.ico",
-		"./src/assets/favicon/apple-touch-icon.png": "/apple-touch-icon.png",
+		"./src/assets/favicon/": "/",
 		"node_modules/@zachleat/details-utils/details-utils.js":
 			"assets/js/details-utils.js",
 	});
